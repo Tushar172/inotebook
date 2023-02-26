64ad8fe81df3c5b06adcfd5be904ec56c45481bf
tanmayVaish <tanmay777leon@gmail.com>
2023-02-26 18:35:24 +0530

    chl gaya

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
-const connectToMongo = require('./db');
-const express = require('express')
-const authRoute = require('./routes/auth')
-const notesRoute = require('./routes/notes')
+const express = require("express");
+const authRoute = require("./routes/auth");
+const notesRoute = require("./routes/notes");
+const mongoose = require("mongoose");
 
-connectToMongo();
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/iNotebook", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected!"))
+  .catch((err) => console.log(err));
 
 // Available Routes
-app.use('/api/auth', authRoute )
-app.use('/api/notes', notesRoute)
-
+app.use("/api/auth", authRoute);
+app.use("/api/notes", notesRoute);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,24 +1,17 @@
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router(); 
-
-const newUser = new User({
-    name: "John Doe",
-    email: "johndoe@example.com",
-    password: "secretpassword"
-  });
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', async (req, res)=>{ 
-    try {
-        //const user = new User(req.body);
-        
-        //await newUser.save();
-        console.log(req);
-        res.status(201).send({ message: 'User created successfully' });
-    } catch (error) {
-        res.status(400).send({ error: error.message });
-    }
+router.post("/", async (req, res) => {
+  try {
+    const user = new User(req.body);
+
+    await user.save();
+    res.status(201).send({ message: "User created successfully" });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
 });
 
 module.exports = router;
